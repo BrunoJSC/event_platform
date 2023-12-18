6e35e4968d81fc6082f234f137e84ad78fac5bab
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-12-18 02:19:21 -0300

    implements stripe

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -18,11 +18,11 @@ export interface IEvent extends Document {
 
 const EventSchema = new Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String },
   location: { type: String },
   createdAt: { type: Date, default: Date.now },
   imageUrl: { type: String, required: true },
-  startDate: { type: Date, default: Date.now },
+  startDateTime: { type: Date, default: Date.now },
   endDateTime: { type: Date, default: Date.now },
   price: { type: String },
   isFree: { type: Boolean, default: false },
